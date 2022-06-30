c9add4f2b56a93ce3ec9b1586515eaa57629885d
choyc4643 <choyc4643@naver.com>
2022-06-30 11:15:52 +0900

    add colors, text_style

---
+++ lib/colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+// const Primary = Color(0xFF00203E);
+// const SubPrimary = Color(0xFF005459);
+// const Background = Color(0xFFC0CEDB);
+// const SubBackground = Color(0xFFF3F4F6);
+
+const Primary = Color(0xFF937F74);
+const SubPrimary = Color(0xFF9EB3B4);
+const Background = Color(0xFFC8CCBF);
+const Button = Color(0xFF7A8071);
+
+
+const Morning = Color(0xFF8FC34F);
+const Lunch = Color(0xFFFFFE54);
+const Dinner = Color(0xFFE99D42);
+const MidNightSnack = Color(0xFFC1281B);
+
+const Toggle1 = Color(0xFF008282);
+const Toggle2 = Color(0xFF00284D);
+const Toggle3 = Color(0xFF004BA1);
+
+const TextBig = Color(0xFFFCF3F3);
+const TextSmall = Color(0xFF000000);
+const TextWeak = Color(0xFF9F9F9F);
+const TextLogOut = Color(0xFFDF2121);
