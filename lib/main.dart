ea578fe1eadd69b91e8c0d9b4c7abd4d9ab432ef
hosoong <lgs512@naver.com>
2022-06-30 23:39:12 +0900

    update home.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'home.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,55 +22,4 @@ void main() async{
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Image Picker Demo',
-      home: MyHomePage(title: 'Image Picker Example'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, this.title}) : super(key: key);
-
-  final String? title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  int count= 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title!),
-      ),
-      
-      body: Center(
-        child: Column(
-          children: [
-            Text('hello, world!'),
-            Text("$count"),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState(() {
-            count++;
-          });
-        },
-        child: const Icon(Icons.add_a_photo),
-      ),
-      
-    );
-  }
-}
