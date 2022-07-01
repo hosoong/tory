97521d0213d87a0fc85620bdb22a0670f446dd79
hosoong <lgs512@naver.com>
2022-07-01 09:47:44 +0900

    update home.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,54 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'home.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart'; //flutter의 package를 가져오는 코드 반드시 필요
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
 );
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+//
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//
+//   runApp(MyApp());
+// }
+
+class MyApp extends StatelessWidget { //MyApp 클래스 선언
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'my first app',
+      home: MyPage(),
+    );
+  }
 }
 
 
+class MyPage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ElevatedButton(
+        child: Text('button'),
+        onPressed: (){
+          createData();
+        },
+      ),
+    );
+  }
+}
+
+void createData(){
+  final usercol=FirebaseFirestore.instance.collection("users").doc("userkey1");
+  usercol.set({
+    "username" : "abc",
+    "age" : 5,
+  });
+}
