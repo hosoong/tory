ef515770dc97c78810d714ee8c178b12d4f92218
hosoong <lgs512@naver.com>
2022-07-01 10:02:09 +0900

    Firebase test code

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,15 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
 import 'home.dart';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart'; //flutter의 package를 가져오는 코드 반드시 필요
-// Import the firebase_core plugin
-import 'package:firebase_core/firebase_core.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,41 +25,3 @@ void main() async{
   runApp(MyApp());
 }
 
-//
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//
-//   runApp(MyApp());
-// }
-
-class MyApp extends StatelessWidget { //MyApp 클래스 선언
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'my first app',
-      home: MyPage(),
-    );
-  }
-}
-
-
-class MyPage extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ElevatedButton(
-        child: Text('button'),
-        onPressed: (){
-          createData();
-        },
-      ),
-    );
-  }
-}
-
-void createData(){
-  final usercol=FirebaseFirestore.instance.collection("users").doc("userkey1");
-  usercol.set({
-    "username" : "abc",
-    "age" : 5,
-  });
-}
