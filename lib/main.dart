2a4e660a1273521d1d347a5c012620d847870796
choyc4643 <choyc4643@naver.com>
2022-07-02 16:24:36 +0900

    UPDATE 스크린유틸 디자인 사이즈 추가, 테마 추가, get routing 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:tory/colors.dart';
+import 'package:tory/story_page.dart';
+import 'package:tory/story_page2.dart';
 import 'firebase_options.dart';
 import 'home.dart';
 
@@ -20,13 +23,37 @@ class MyApp extends StatelessWidget {
   //MyApp 클래스 선언
   Widget build(BuildContext context) {
     return ScreenUtilInit(
+      designSize: Size(414,896),
       builder: (context, child) => GetMaterialApp(
+        theme: ThemeData(
+          primarySwatch: MaterialColor(background2.value, {
+            50: Color(0xFF174378),
+            100: Color(0xFF174378),
+            200: Color(0xFF174378),
+            300: Color(0xFF174378),
+            400: Color(0xFF174378),
+            500: Color(0xFF174378),
+            600: Color(0xFF174378),
+            700: Color(0xFF174378),
+            800: Color(0xFF174378),
+            900: Color(0xFF174378),
+          }),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
         title: 'my first app',
-        home: HomePage(),
+        home: StoryPage(),
         getPages: [
           GetPage(
             name: '/',
             page: () => HomePage(),
+          ),
+          GetPage(
+            name: '/story',
+            page: () => StoryPage(),
+          ),
+          GetPage(
+            name: '/story2',
+            page: () => StoryPage2(),
           ),
         ],
       ),
