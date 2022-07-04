d3cdabf57580eab44f3b98bab660c1a236bb2fdf
choyc4643 <choyc4643@naver.com>
2022-07-04 15:23:02 +0900

    [UPDATE] colors.dart 수정

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 
-const Primary = Color(0xFF6FCDB1);
+const Primary = Color(0xFF49BBA3);
 const SubPrimary = Color(0xFFB0DEBD);
 const tertiary = Color(0xFFF6B87F);
 const neutral = Color(0xFFFAF7EF);
 
-const text1 = Color(0xFF2F2F2F);
-const text2 = Color(0xFF1F1F1F);
+const text_on_color = Color(0xFF2F2F2F);
+const text_on_background = Color(0xFF1F1F1F);
+const text_on_button = Color(0xFFFFFFFF);
 
 //투명도 20프로?인 것 같은데 어케하지 ㅎㅎ
 const layer_blur = Color.fromRGBO(0, 0, 0, 0.2);
 
 
-const statement1 = Color(0xFFD92C23);
-const statement2 = Color(0xFF008AD6);
+const error = Color(0xFFD92C23);
+const warning = Color(0xFFFFCC48);
 
 const background1 = Color(0xFFEBEBEB);
-const background2 = Color(0xFFFDFCF9);
+const background2 = Color(0xFFF8F6F4);
 const background3 = Color(0xFFFFFFFF);
