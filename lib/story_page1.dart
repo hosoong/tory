5461c1f3b21aeb33bae8dea0da907a5d2b3a2114
hosoong <lgs512@naver.com>
2022-07-05 17:23:27 +0900

    [Update] story_page.dart 코드 에러 수정

--- lib/story_page1.dart
+++ lib/story_page1.dart
@@ -27,24 +27,30 @@ class StoryPage extends StatelessWidget {
             Material(
               elevation: 4,
               child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 14, vertical: 68),
-                color: neutral,
-                width: 333.w,
-                // height: 341.h,
-                child: Text('\"오래전부터 좋아하는 ㅁㅁㅁ와 나는 지금 썸을 타고 있다. \n내가 생각하기에는 이건 분명... 썸이다. \n영일대까지 가서 저녁을 먹었다~ \n대박대박!~~~ 밤 산책도 몇번 같이 했다. \n빨리 사귀고 싶어 ㅠㅠ 어떡하지?? \n얼른 고백해버리고 싶은데... \n방법을 모르겠어! 난 뚝딱이란 말이야..\"',textAlign: TextAlign.center, style: bodystyle,)),
+                  padding: EdgeInsets.symmetric(horizontal: 14, vertical: 68),
+                  color: neutral,
+                  width: 333.w,
+                  // height: 341.h,
+                  child: Text(
+                    '\"오래전부터 좋아하는 ㅁㅁㅁ와 나는 지금 썸을 타고 있다. \n내가 생각하기에는 이건 분명... 썸이다. \n영일대까지 가서 저녁을 먹었다~ \n대박대박!~~~ 밤 산책도 몇번 같이 했다. \n빨리 사귀고 싶어 ㅠㅠ 어떡하지?? \n얼른 고백해버리고 싶은데... \n방법을 모르겠어! 난 뚝딱이란 말이야..\"',
+                    textAlign: TextAlign.center,
+                    style: bodystyle,
+                  )),
             ),
             SizedBox(height: 200.h),
             ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  fixedSize: Size(190.w,50.h),
-                  primary: Primary, // background
+                  fixedSize: Size(190.w, 50.h),
+                  primary: Primary600, // background
                   onPrimary: Colors.white, // foreground
                 ),
-              onPressed: (){
-                Get.toNamed('/story2');
-              }, 
-              child: Text("토리 시작하기",style: TextStyle(fontSize: 19),))
-
+                onPressed: () {
+                  Get.toNamed('/story2');
+                },
+                child: Text(
+                  "토리 시작하기",
+                  style: TextStyle(fontSize: 19),
+                ))
           ],
         ),
       ),
