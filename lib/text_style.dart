db3f708e855810c5a1e06e8b438b078f1d8010b6
choyc4643 <choyc4643@naver.com>
2022-07-06 13:48:29 +0900

    [UPDATE] 컬러, 텍스트 수정

--- lib/text_style.dart
+++ lib/text_style.dart
@@ -1,31 +1,181 @@
 import 'package:flutter/material.dart';
 
-//heading
-TextStyle headingstyle({double? size, Color? color}) {
+
+TextStyle headline1({Color? color}){
   return TextStyle(
-    fontSize: size,
-    fontFamily: 'Bold',
     color: color,
+    fontFamily: "NanumSquare",
+    fontSize: 24,
+    fontWeight: FontWeight.w800,
   );
 }
 
-
-TextStyle subtitlestyle({double? size, Color? color, FontWeight? weight}) {
+TextStyle headline2({Color? color}){
   return TextStyle(
-    fontSize: size,
-    fontFamily: 'Regular',
-    fontWeight: weight,
     color: color,
+    fontFamily: "NanumSquare",
+    fontSize: 20,
+    fontWeight: FontWeight.w800,
   );
 }
 
-TextStyle bodystyle = TextStyle(
+TextStyle headline3({Color? color}){
+  return TextStyle(
+    color: color,
+    fontFamily: "NanumSquare",
     fontSize: 18,
-    color: Colors.black,
+    fontWeight: FontWeight.w700,
   );
+}
 
-TextStyle titlestyle = TextStyle(
-  fontSize: 18,
-  color: Colors.black,
-  fontWeight: FontWeight.bold,
-);
+TextStyle headline4({Color? color}){
+  return TextStyle(
+    color: color,
+    fontFamily: "NanumSquare",
+    fontSize: 16,
+    fontWeight: FontWeight.w800,
+    height: 1.15,
+  );
+}
+
+TextStyle headline5({Color? color}){
+  return TextStyle(
+    color: color,
+    fontFamily: "NanumSquare",
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+  );
+}
+
+TextStyle subtitle1({Color? color}){
+  return TextStyle(
+    color: color,
+    fontFamily: "NanumSquare",
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    height: 1.4
+  );
+}
+
+TextStyle subtitle2({Color? color}){
+  return TextStyle(
+    color: color,
+    fontFamily: "NanumSquare",
+    fontSize: 12,
+    fontWeight: FontWeight.w800,
+  );
+}
+
+TextStyle subtitle3({Color? color}){
+  return TextStyle(
+    color: color,
+    fontFamily: "NanumSquare",
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+  );
+}
+
+TextStyle subtitle4({Color? color}){
+  return TextStyle(
+    color: color,
+    fontFamily: "NanumSquare",
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+}
+
+TextStyle button1({Color? color}){
+  return TextStyle(
+    color: color,
+    fontFamily: "NanumSquare",
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    height: 1.6,
+  );
+}
+TextStyle button2({Color? color}){
+  return TextStyle(
+    color: color,
+    fontFamily: "NanumSquare",
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+  );
+}
+
+TextStyle button3({Color? color}){
+  return TextStyle(
+    color: color,
+    fontFamily: "NanumSquare",
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+  );
+}
+
+TextStyle button4({Color? color}){
+  return TextStyle(
+    color: color,
+    fontFamily: "NanumSquare",
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+  );
+}
+
+TextStyle button5({Color? color}){
+  return TextStyle(
+    color: color,
+    fontFamily: "NanumSquare",
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    textBaseline: TextBaseline.alphabetic,
+  );
+}
+
+TextStyle body1({Color? color}){
+  return TextStyle(
+    color: color,
+    fontFamily: "NanumSquare",
+    fontSize: 18,
+    fontWeight: FontWeight.w800,
+    height: 1.4
+  );
+}
+
+TextStyle body2({Color? color}){
+  return TextStyle(
+    color: color,
+    fontFamily: "NanumSquare",
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    height: 1.4
+  );
+}
+
+TextStyle body3({Color? color}){
+  return TextStyle(
+    color: color,
+    fontFamily: "NanumSquare",
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 1.4
+  );
+}
+
+TextStyle body4({Color? color}){
+  return TextStyle(
+    color: color,
+    fontFamily: "NanumSquare",
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 1.4
+  );
+}
+
+TextStyle body5({Color? color}){
+  return TextStyle(
+    color: color,
+    fontFamily: "NanumSquare",
+    fontSize: 10,
+    fontWeight: FontWeight.w300,
+    height: 1.4
+  );
+}
