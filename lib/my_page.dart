c10a980cc7bde4c0b9f92d74525108765db84376
Yeggggi <49176310+Yeggggi@users.noreply.github.com>
2022-07-07 09:20:22 +0900

    [UPDATE] my page UI
    [UPDATE] my page UI

--- lib/my_page.dart
+++ lib/my_page.dart
@@ -6,9 +6,14 @@ import 'colors.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import 'package:get/get.dart';
 
-class MyPage extends StatelessWidget {
+class MyPage extends StatefulWidget {
   const MyPage({Key? key}) : super(key: key);
 
+  @override
+  State<MyPage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +28,8 @@ class MyPage extends StatelessWidget {
               children: [
                 myPageHeader(context),
                 myPageMoaBogi(context),
+                myPagerecentTory(context),
+                myPageSetting(context),
               ],
             ),
           )
@@ -197,9 +204,7 @@ Widget myPageHeader(BuildContext context) {
         right: 24,
         child: Center(
           child: IconButton(
-              onPressed: () {
-
-              },
+              onPressed: () {},
               icon: Icon(
                 Icons.arrow_forward_ios,
                 color: Colors.white,
@@ -219,13 +224,15 @@ void openBottomSheet(BuildContext context) {
       color: neutral,
       child: Stack(
         children: [
-          Image.asset('assets/snowinfo.png',
-            width: MediaQuery.of(context).size.width,),
+          Image.asset(
+            'assets/snowinfo.png',
+            width: MediaQuery.of(context).size.width,
+          ),
           Positioned(
             top: 14,
             right: 46,
             child: Container(
-                color: neutral,
+              color: neutral,
               child: IconButton(
                   onPressed: () {
                     Get.back();
@@ -237,7 +244,6 @@ void openBottomSheet(BuildContext context) {
                   )),
             ),
           ),
-
         ],
       ),
     ),
@@ -283,14 +289,20 @@ Future<void> _showSnowInfo(BuildContext context) async {
 }
 
 //모아보기
-myPageMoaBogi(BuildContext context){
+myPageMoaBogi(BuildContext context) {
   return Row(
     children: [
-      SizedBox(width: 24,),
+      SizedBox(
+        width: 24,
+      ),
       Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('모아보기',style: headline3(color: text_on_color),),
+          Text(
+            '모아보기',
+            style: headline3(color: text_on_color),
+          ),
+          SizedBox(height: 12,),
           Container(
             width: 366,
             height: 118,
@@ -301,59 +313,222 @@ myPageMoaBogi(BuildContext context){
             child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                SizedBox(width: 24,),
+                SizedBox(
+                  width: 24,
+                ),
                 //시나리오
                 InkWell(
-                    child: Column(
-                      children: [
-                        Container(height: 76,width: 76,),
-                        Expanded(child: Text('시나리오',style: headline5(color: text_on_color),)),
-                      ],
-                    ),
-                    onTap: (){
-                      print('시나리오');
-                    },
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 76,
+                        width: 76,
+                      ),
+                      Expanded(
+                          child: Text(
+                        '시나리오',
+                        style: headline5(color: text_on_color),
+                      )),
+                    ],
                   ),
-                SizedBox(width: 15,),
+                  onTap: () {
+                    print('시나리오');
+                  },
+                ),
+                SizedBox(
+                  width: 15,
+                ),
                 VerticalDivider(
                   indent: 23,
                   endIndent: 22,
                 ),
-                SizedBox(width: 10,),
+                SizedBox(
+                  width: 10,
+                ),
                 InkWell(
                   child: Column(
                     children: [
-                      Container(height: 76,width: 76,),
-                      Expanded(child: Text('답변',style: headline5(color: text_on_color),)),
+                      Container(
+                        height: 76,
+                        width: 76,
+                      ),
+                      Expanded(
+                          child: Text(
+                        '답변',
+                        style: headline5(color: text_on_color),
+                      )),
                     ],
                   ),
-                  onTap: (){
+                  onTap: () {
                     print('답변');
                   },
                 ),
-                SizedBox(width: 10,),
+                SizedBox(
+                  width: 10,
+                ),
                 VerticalDivider(
                   indent: 23,
                   endIndent: 22,
                 ),
-                SizedBox(width: 15,),
+                SizedBox(
+                  width: 15,
+                ),
                 InkWell(
                   child: Column(
                     children: [
-                      Container(height: 76,width: 76,),
-                      Expanded(child: Text('투표결과',style: headline5(color: text_on_color),)),
+                      Container(
+                        height: 76,
+                        width: 76,
+                      ),
+                      Expanded(
+                          child: Text(
+                        '투표결과',
+                        style: headline5(color: text_on_color),
+                      )),
                     ],
                   ),
-                  onTap: (){
+                  onTap: () {
                     print('투표결과');
                   },
                 ),
-                SizedBox(width: 20,)
+                SizedBox(
+                  width: 20,
+                )
               ],
             ),
           )
         ],
       ),
     ],
+  );
+}
+
+//최근 토리
+//TODO: 빌더로 바꾸
+myPagerecentTory(BuildContext context) {
+  PageController? _pageController;
+
+  List<String> images = [
+    "assets/image13.png",
+    "assets/image14.png",
+    "assets/image12.png",
+    "assets/love.png",
+    "assets/snow_5m.png",
+  ];
+
+  int activePage = 1;
+  int Page = 5;
+
+  return Column(
+    children: <Widget>[
+      SizedBox(
+        height: 32,
+      ),
+      Row(
+        children: [
+          SizedBox(
+            width: 24,
+          ),
+          Text(
+            '지연님의 최근 토리',
+            style: headline3(color: text_on_color),
+          ),
+          Icon(
+            Icons.clear,
+            color: Colors.black,
+            size: 18,
+          )
+        ],
+      ),
+      Container(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              child: SizedBox(
+                height: 200,
+                child: PageView.builder(
+                    itemCount: 4,
+                    pageSnapping: true,
+                    controller: _pageController,
+                    onPageChanged: (page) {
+                      // setState(() {
+                      //   Page = page;
+                      // });
+                    },
+                    itemBuilder: (context, pagePosition) {
+                      return Container(
+                        margin: EdgeInsets.all(10),
+                        // child: Image.network(images[pagePosition]),
+                        child: Image.asset(images[pagePosition]),
+                      );
+                    }),
+              ),
+            ),
+          ],
+        ),
+      )
+    ],
+  );
+}
+
+myPageSetting(BuildContext context) {
+  return Expanded(
+    child: Container(
+      width: 414,
+      height: 158,
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          InkWell(
+            child: Container(
+              width: 414,
+              height: 50,
+              child: Row(
+                children: [
+                  SizedBox(width: 24,),
+                  Text('설정', style: headline5(color: text_on_color)),
+                ],
+              ),
+            ),
+            onTap: () {
+              print('설정');
+            },
+          ),
+          InkWell(
+            child: Container(
+              width: 414,
+              height: 50,
+              child: Row(
+                children: [
+                  SizedBox(width: 24,),
+                  Text('공지사항', style: headline5(color: text_on_color)),
+                ],
+              ),
+            ),
+            onTap: () {
+              print('공지사항');
+            },
+          ),
+          InkWell(
+            child: Container(
+              width: 414,
+              height: 50,
+              child: Row(
+                children: [
+                  SizedBox(width: 24,),
+                  Text('문의사항', style: headline5(color: text_on_color)),
+                ],
+              ),
+            ),
+            onTap: () {
+              print('문의사항');
+            },
+          ),
+        ],
+      ),
+    ),
   );
 }
