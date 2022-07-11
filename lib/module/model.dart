5adabd1e0344e8e07e0d0839654d38f5877506a5
Cho <choyc4643@naver.com>
2022-07-11 13:20:53 +0900

    [ADD] model.dart

---
+++ lib/module/model.dart
@@ -0,0 +1,23 @@
+class Story{
+  Story({
+    required this.category,
+    required this.doc_id,
+    required this.day,
+    required this.end_index,
+    required this.end_type,
+    required this.user_id,
+    required this.like,
+    required this.title,
+    required this.explain,
+  });
+
+  final String category;
+  final String doc_id;
+  final String day;
+  final String end_index;
+  final String end_type;
+  final String user_id;
+  final String like;
+  final String title;
+  final String explain;
+}
